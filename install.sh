377a8c0a2c8c6621291580d59929dc34954800a4
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2019-04-05 10:02:52 +0700

    добавил git-mvrep в install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 DIR=`pwd`
 sudo ln -s -T $DIR/git-rmsub   /usr/local/bin/git-rmsub    
 sudo ln -s -T $DIR/git-newrep  /usr/local/bin/git-newrep  
+sudo ln -s -T $DIR/git-mvrep   /usr/local/bin/git-mvrep
