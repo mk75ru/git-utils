c7f2d0e057f960fce106e75d4557951fc6575e75
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2019-08-12 10:01:29 +0700

    доработал git-mvrep2

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 DIR=`pwd`
+echo "Запускаем из каталога где хранятся исходные файлы"
 sudo ln -s -T $DIR/git-rmsub   /usr/local/bin/git-rmsub    
 sudo ln -s -T $DIR/git-newrep  /usr/local/bin/git-newrep  
 sudo ln -s -T $DIR/git-mvrep   /usr/local/bin/git-mvrep
+sudo ln -s -T $DIR/git-mvrep2  /usr/local/bin/git-mvrep2
