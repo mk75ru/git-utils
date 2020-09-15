4c9d4bff1df4255aec6a38f5c6bf121491597fe4
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2020-09-15 11:48:36 +0700

    добавил git-rmvcs

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ sudo ln -s -T $DIR/git-rmsub   /usr/local/bin/git-rmsub
 sudo ln -s -T $DIR/git-newrep  /usr/local/bin/git-newrep  
 sudo ln -s -T $DIR/git-mvrep   /usr/local/bin/git-mvrep
 sudo ln -s -T $DIR/git-mvrep2  /usr/local/bin/git-mvrep2
+sudo ln -s -T $DIR/git-rmvcs   /usr/local/bin/git-rmvcs
