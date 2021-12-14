d4643a034fc32e1558131b9ac5d06448f20b0d87
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2021-12-14 17:25:13 +0700

    добавил файлов

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ sudo ln -s -T $DIR/git-newrep  /usr/local/bin/git-newrep
 sudo ln -s -T $DIR/git-mvrep   /usr/local/bin/git-mvrep
 sudo ln -s -T $DIR/git-mvrep2  /usr/local/bin/git-mvrep2
 sudo ln -s -T $DIR/git-rmvcs   /usr/local/bin/git-rmvcs
+sudo ln -s -T $DIR/git-mvrep-to-github  /usr/local/bin/git-mvrep-to-github
